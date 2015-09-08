e53ea84fe99f189b95c3ed1ac09bac799c4f9d9c
Ian Hamilton <ian@colorcrate.com>
2015-09-08 19:32:17 -0400

    Events page, events single markup and styling.

---
+++ event-single.php
@@ -0,0 +1,90 @@
+<?php
+  // Variable setup for this page.
+  // If and when this template is transferred to a CMS,
+  // these variables should hopefully be replaced by
+  // something CMS-driven.
+
+  $currentPage = 'events';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    
+    <?php include('includes/head.php'); ?>
+
+    <title>Home – The Carmel Institute of Russian Culture &amp; History</title>
+
+  </head>
+  <body class="<?php echo $currentPage; ?>">
+    
+    <?php include('includes/site-header.php'); ?>
+
+    <!-- BEGIN PAGE TITLE -->
+    <header class="page-title page-title--has-image">
+      <div class="page-title__background" style="background-image: url(img/event-placeholder-733x418.jpg);"></div>
+      <div class="container">
+        <h1>Mosfilm Screens “Some Russian Movie” at E Street Cinema</h1>
+        <div class="page-title__info-container">
+          <span class="page-title__info">July 3, 2015</span>
+          <span class="page-title__info">555 11th St NW<br />Washington, DC 20004</span>
+        </div>
+      </div>
+    </header>
+    <!-- END PAGE TITLE -->
+
+    <!-- BEGIN TWO COLUMN CONTENT AREA -->
+    <div class="content-area content-area--two-col  container">
+      <div class="content-area__column content-area__column--wide">
+        <!-- Basic HTML content should be placed here. Classes should not be used unless absolutely necessary -->
+        <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sem enim, aliquam elementum egestas in, ultricies a orci.</h3>
+        <p>Vestibulum elementum lacus at sem dignissim fermentum. Cras justo sem, condimentum nec felis vel, egestas vehicula lorem. Morbi hendrerit mauris purus, vel molestie sem tincidunt elementum. Praesent consequat massa eros, vel vestibulum eros mollis sed. Mauris massa massa, ultrices a cursus malesuada, facilisis et nulla. Nulla lacinia tristique eros sit amet lobortis. Ut vel rutrum urna. Integer scelerisque dapibus nisl eget tristique. Aliquam rutrum posuere massa, ac posuere ex tincidunt in. Etiam luctus tempor risus elementum pulvinar. Ut a consectetur risus.</p>
+        <p>Curabitur vulputate, nibh ultricies malesuada elementum, lorem leo blandit quam, et sodales tellus mauris pretium justo. Cras congue interdum magna, quis porta dui egestas ac. Sed eu enim nec ex porta euismod quis vitae nunc. Vestibulum sed pulvinar dui, at tincidunt justo. Suspendisse mi mauris, rutrum et nunc consequat, porta pellentesque ante. Etiam vel massa et nulla congue fringilla. In sit amet odio consequat, cursus sem sed, mollis urna.</p>
+      </div>
+      <div class="content-area__column content-area__column--narrow">
+        <h3 class="content-area__title">
+          <span>RSVP</span>
+        </h3>
+        <p>Lorem ipsum dolor sit email to <a href="#">someone@american.edu</a> or sed in semper quam, cras sit amet rutum felis. Maximum five seats per RSVP. <strong>Please RSVP no later than July 28, 2015.</strong></p>
+        <a class="btn btn-secondary">
+          <span>RSVP</span>
+        </a>
+
+        <h3 class="content-area__title">
+          <span>Partner Organizations</span>
+        </h3>
+        <div class="content-area__logo">
+          <a href="#">
+            <img src="http://placehold.it/100x100" alt="name" />
+          </a>
+        </div>
+        <div class="content-area__logo">
+          <a href="#">
+            <img src="http://placehold.it/100x100" alt="name" />
+          </a>
+        </div>
+        <div class="content-area__logo">
+          <a href="#">
+            <img src="http://placehold.it/100x100" alt="name" />
+          </a>
+        </div>
+        <div class="content-area__logo">
+          <a href="#">
+            <img src="http://placehold.it/100x100" alt="name" />
+          </a>
+        </div>
+
+      </div>
+    </div>
+    <!-- END TWO COLUMN CONTENT AREA -->
+
+
+    <?php include('includes/site-footer.php'); ?>
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+  </body>
+</html>
--- events.php
+++ events.php
@@ -33,7 +33,7 @@
       <div class="row">
         <div class="featured-event  col-xs-12 col-sm-8">
           <h3>
-            <a href="#">Mosfilm screens “Some Russian Movie” at E Street Cinema</a>
+            <a href="event-single.php">Mosfilm screens “Some Russian Movie” at E Street Cinema</a>
           </h3>
           <span class="featured-event__date">July 3, 2015</span>
           <img class="featured-event__image" src="img/event-placeholder-733x418.jpg">
@@ -41,7 +41,61 @@
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In iaculis lorem nec ipsum gravida, et finibus elit sollicitudin. Curabitur vel felis condimentum, placerat diam id, aliquam sapien. Nullam efficitur neque id augue maximus suscipit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed sit amet nunc massa. Suspendisse auctor enim massa, aliquet dictum sapien aliquet sed.</p>
             <p>Aenean posuere odio lacus, quis blandit sapien accumsan at. Ut rutrum vulputate tellus. Integer sed orci nec ipsum suscipit sodales in vel purus. Sed ultrices est vitae justo vehicula, sed pellentesque dolor commodo.</p>
           </div>
-          <a class="btn btn-primary">Learn More &amp; RSVP</a>
+          <a class="btn btn-primary" href="event-single.php">Learn More &amp; RSVP</a>
+        </div>
+
+        <div class="feed  col-xs-12 col-sm-4">
+          <h3 class="feed__title">
+            <span>Spring 2015 Calendar</span>
+          </h3>
+          <ul class="feed__list">
+            <li class="feed__item">
+              <h4>
+                <a href="#">MGIMO International Summer School</a>
+              </h4>
+              <p class="feed__item__description">
+                Moscow, Russia<br />
+                Session I: 22 June - 12 July 2015<br />
+                Session II: 13 July - 2 August 2015
+              </p>
+            </li>
+            <li class="feed__item">
+              <h4>
+                <a href="#">Higher School of Economics International Summer University</a>
+              </h4>
+              <p class="feed__item__description">
+                Moscow and St. Petersburg, Russia<br />
+                22 June - 8 August 2015
+              </p>
+            </li>
+            <li class="feed__item">
+              <h4>
+                <a href="#">AU/Carmel Institute course: Literary Russia</a>
+              </h4>
+              <p class="feed__item__description">
+                Select Consortium students<br />
+                13 to 23 July 2015<br />
+                St. Petersburg and Moscow, Russia
+              </p>
+            </li>
+            <li class="feed__item">
+              <h4>
+                <a href="#">Bard-Smolny Institute</a>
+              </h4>
+              <p class="feed__item__description">
+                St. Petersburg, Russia<br />
+                Summer Terms 2015
+              </p>
+            </li>
+            <li class="feed__item">
+              <h4>
+                <a href="#">American Councils' Business Russian Language and Internship</a>
+              </h4>
+              <p class="feed__item__description">
+                Moscow or St. Petersburg, Russia<br />Summer 2015
+              </p>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
@@ -54,11 +108,12 @@
     <div class="callout-list callout-list--image">
       <div class="container">
 
-        <div class="hr"><div class="flourish"></div></div>
+        <h3 class="callout-list__title--bar">
+          <span>Past Event Galleries</span>
+        </h3>
 
         <div class="row">
           <div class="callout-list__column  col-xs-12 col-sm-4">
-            <span class="callout-list__title">Upcoming Event</span>
             <div class="callout-list__image">
               <img src="http://placehold.it/350x233" alt="alt-text" />
             </div>
@@ -67,13 +122,11 @@
                 <a href="#">Title of item lorem ipsum dolor sit amet</a>
               </h3>
               <span class="callout-list__date">July 3, 2015</span>
-              <p class="callout-list__description">Description of item. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu enim vel felis scelerisque imperdiet.</p>
               <p><a href="#" class="btn btn-primary" role="button">Call to action</a></p>
             </div>
           </div>
 
           <div class="callout-list__column  col-xs-12 col-sm-4">
-            <span class="callout-list__title">Study Abroad</span>
             <div class="callout-list__image">
               <img src="http://placehold.it/350x233" alt="alt-text" />
             </div>
@@ -82,13 +135,11 @@
                 <a href="#">Title of item lorem ipsum dolor sit amet</a>
               </h3>
               <span class="callout-list__date">July 3, 2015</span>
-              <p class="callout-list__description">Description of item. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu enim vel felis scelerisque imperdiet.</p>
               <p><a href="#" class="btn btn-primary" role="button">Call to action</a></p>
             </div>
           </div>
 
           <div class="callout-list__column  col-xs-12 col-sm-4">
-            <span class="callout-list__title">About the Institute</span>
             <div class="callout-list__image">
               <img src="http://placehold.it/350x233" alt="alt-text" />
             </div>
@@ -97,97 +148,98 @@
                 <a href="#">Title of item lorem ipsum dolor sit amet</a>
               </h3>
               <span class="callout-list__date">July 3, 2015</span>
-              <p class="callout-list__description">Description of item. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu enim vel felis scelerisque imperdiet.</p>
               <p><a href="#" class="btn btn-primary" role="button">Call to action</a></p>
             </div>
           </div>
+
+          <div class="callout-list__column  col-xs-12 col-sm-4">
+            <div class="callout-list__image">
+              <img src="http://placehold.it/350x233" alt="alt-text" />
+            </div>
+            <div class="callout-list__caption">
+              <h3>
+                <a href="#">Title of item lorem ipsum dolor sit amet</a>
+              </h3>
+              <span class="callout-list__date">July 3, 2015</span>
+              <p><a href="#" class="btn btn-primary" role="button">Call to action</a></p>
+            </div>
+          </div>
+
+          <div class="callout-list__column  col-xs-12 col-sm-4">
+            <div class="callout-list__image">
+              <img src="http://placehold.it/350x233" alt="alt-text" />
+            </div>
+            <div class="callout-list__caption">
+              <h3>
+                <a href="#">Title of item lorem ipsum dolor sit amet</a>
+              </h3>
+              <span class="callout-list__date">July 3, 2015</span>
+              <p><a href="#" class="btn btn-primary" role="button">Call to action</a></p>
+            </div>
+          </div>
+
+          <div class="callout-list__column  col-xs-12 col-sm-4">
+            <div class="callout-list__image">
+              <img src="http://placehold.it/350x233" alt="alt-text" />
+            </div>
+            <div class="callout-list__caption">
+              <h3>
+                <a href="#">Title of item lorem ipsum dolor sit amet</a>
+              </h3>
+              <span class="callout-list__date">July 3, 2015</span>
+              <p><a href="#" class="btn btn-primary" role="button">Call to action</a></p>
+            </div>
+          </div>
+
+          <div class="callout-list__column  col-xs-12 col-sm-4">
+            <div class="callout-list__image">
+              <img src="http://placehold.it/350x233" alt="alt-text" />
+            </div>
+            <div class="callout-list__caption">
+              <h3>
+                <a href="#">Title of item lorem ipsum dolor sit amet</a>
+              </h3>
+              <span class="callout-list__date">July 3, 2015</span>
+              <p><a href="#" class="btn btn-primary" role="button">Call to action</a></p>
+            </div>
+          </div>
+
+          <div class="callout-list__column  col-xs-12 col-sm-4">
+            <div class="callout-list__image">
+              <img src="http://placehold.it/350x233" alt="alt-text" />
+            </div>
+            <div class="callout-list__caption">
+              <h3>
+                <a href="#">Title of item lorem ipsum dolor sit amet</a>
+              </h3>
+              <span class="callout-list__date">July 3, 2015</span>
+              <p><a href="#" class="btn btn-primary" role="button">Call to action</a></p>
+            </div>
+          </div>
+
+          <div class="callout-list__column  col-xs-12 col-sm-4">
+            <div class="callout-list__image">
+              <img src="http://placehold.it/350x233" alt="alt-text" />
+            </div>
+            <div class="callout-list__caption">
+              <h3>
+                <a href="#">Title of item lorem ipsum dolor sit amet</a>
+              </h3>
+              <span class="callout-list__date">July 3, 2015</span>
+              <p><a href="#" class="btn btn-primary" role="button">Call to action</a></p>
+            </div>
+          </div>
+
+          <div class="load-more col-xs-12">
+            <a class="load-more__button  btn btn-secondary" href="#">
+              <span>Load More</span>
+            </a>
+          </div>
+
         </div> <!-- .row -->
-
       </div> <!-- .container -->
     </div> <!-- .callouts -->
-    <!-- END EVENTS CALLOUT ITEMS -->
-
-
-    <!-- BEGIN HOMEPAGE TEXT LIST -->
-    <div class="callout-list callout-list__text">
-      <div class="container">
-
-        <div class="hr"><div class="flourish"></div></div>
-
-        <div class="row">
-          <div class="callout-list__column  col-xs-12 col-sm-4">
-            <span class="callout-list__title">Recent News</span>
-            <div class="callout-list__caption">
-              <h3>
-                <a href="#">Title of item lorem ipsum dolor sit amet</a>
-              </h3>
-              <span class="callout-list__date">July 3, 2015</span>
-              <p class="callout-list__description">Description of item. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu enim vel felis scelerisque imperdiet... <a href="#">read more.</a></p>
-            </div>
-            <div class="callout-list__caption">
-              <h3>
-                <a href="#">Title of item lorem ipsum dolor sit amet</a>
-              </h3>
-              <span class="callout-list__date">July 3, 2015</span>
-              <p class="callout-list__description">Description of item. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu enim vel felis scelerisque imperdiet... <a href="#">read more.</a></p>
-            </div>
-          </div>
-
-          <div class="callout-list__column  col-xs-12 col-sm-4">
-            <span class="callout-list__title">Study Abroad Opportunities</span>
-            <div class="callout-list__caption">
-              <h3>
-                <a href="#">Title of item lorem ipsum dolor sit amet</a>
-              </h3>
-              <span class="callout-list__date">July 3, 2015</span>
-              <p class="callout-list__description">Description of item. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu enim vel felis scelerisque imperdiet... <a href="#">read more.</a></p>
-            </div>
-            <div class="callout-list__caption">
-              <h3>
-                <a href="#">Title of item lorem ipsum dolor sit amet</a>
-              </h3>
-              <span class="callout-list__date">July 3, 2015</span>
-              <p class="callout-list__description">Description of item. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu enim vel felis scelerisque imperdiet... <a href="#">read more.</a></p>
-            </div>
-          </div>
-
-          <div class="callout-list__column  col-xs-12 col-sm-4">
-            <span class="callout-list__title">Friends &amp; Venues</span>
-            <div class="callout-list__logo  col-xs-6">
-              <a href="#">
-                <img src="http://placehold.it/100x100" alt="name" />
-              </a>
-            </div>
-            <div class="callout-list__logo  col-xs-6">
-              <a href="#">
-                <img src="http://placehold.it/100x100" alt="name" />
-              </a>
-            </div>
-            <div class="callout-list__logo  col-xs-6">
-              <a href="#">
-                <img src="http://placehold.it/100x100" alt="name" />
-              </a>
-            </div>
-            <div class="callout-list__logo  col-xs-6">
-              <a href="#">
-                <img src="http://placehold.it/100x100" alt="name" />
-              </a>
-            </div>
-            <div class="callout-list__logo  col-xs-6">
-              <a href="#">
-                <img src="http://placehold.it/100x100" alt="name" />
-              </a>
-            </div>
-            <div class="callout-list__logo  col-xs-6">
-              <a href="#">
-                <img src="http://placehold.it/100x100" alt="name" />
-              </a>
-            </div>
-          </div>
-        </div> <!-- .row -->
-      </div> <!-- .container -->
-    </div> <!-- .text-list -->
-    <!-- END HOMEPAGE TEXT LIST -->   
+    <!-- END EVENTS CALLOUT ITEMS -->  
 
     <?php include('includes/site-footer.php'); ?>
 
